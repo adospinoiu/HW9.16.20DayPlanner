a0b45b3740ce9d51587a25f3c9fee12e7aae8b8e
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-09-12 06:45:46 -0700

    Able to display the first row (i.e. time, input area, save button)

--- script.js
+++ script.js
@@ -7,29 +7,37 @@ let array = [];
 array.push(currentTime);
 console.log(array);
 
+// Array of hours considered workHours that will be displayed
 let workHours = ["07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM"];
 
-let container = $(".container");
-let time = "";
-time = "5:00AM";
+// Links to the 'container' <div> on the html. ***Delete CLASS***
+let container = $(".container").addClass("past");
+// let time = "";  ****DELETE ME IF NOT NEEDED****
+// time = "5:00AM";   ****DELETE ME IF NOT NEEDED****
 
+// Creates a new ROW that will have the workHour, Input, and saveButton
 let newRow = $("<section>").addClass("row");
-let timeColumn = $("<div>").addClass("col-md-4");
-timeColumn.text(time);
+
+// Creates the <div> in the new ROW that will display the workHour
+let timeColumn = $("<div>").addClass("col-md-2 hour time-block");
+timeColumn.text(workHours[0]); //previously 'time'
 newRow.append(timeColumn);
 
-let inputColumn = $("<div>").addClass("col-md-5");
-let input = $('<input type= "text">');
-input.addClass("input");
-input.attr("id", time);
+// Creates the <div> in the new ROW that will display the text input area
+let inputColumn = $("<div>").addClass("col-md-8");
+let input = $('<input type = "text">');
+input.addClass("input textarea");
+input.attr("id", workHours[0]); //previously workHours was 'time'
 inputColumn.append(input);
 newRow.append(inputColumn);
 
-let buttonColumn = $("<div>").addClass("col-md-3");
-let saveButton = $("<button>").addClass("save-btn");
-saveButton.attr("date-time", time);
+// Creates the <div> in the new ROW that will display the saveButton
+let buttonColumn = $("<div>").addClass("col-md-2");
+let saveButton = $("<button>").addClass("save-btn saveBtn");
+saveButton.attr("date-time", workHours[0]); //previously workHours was 'time'
 saveButton.text("Save");
 buttonColumn.append(saveButton);
 newRow.append(buttonColumn);
 
+// Displays the new ROW (i.e. workHour, inputText, saveButton)
 container.append(newRow);
