d308a8b81e29ee4591650b6b95b0b37c81b84d41
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-09-12 06:50:41 -0700

    Created for loop that adds all the rows based on number of workHours in array

--- script.js
+++ script.js
@@ -12,32 +12,34 @@ let workHours = ["07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12
 
 // Links to the 'container' <div> on the html. ***Delete CLASS***
 let container = $(".container").addClass("past");
-// let time = "";  ****DELETE ME IF NOT NEEDED****
-// time = "5:00AM";   ****DELETE ME IF NOT NEEDED****
 
-// Creates a new ROW that will have the workHour, Input, and saveButton
-let newRow = $("<section>").addClass("row");
+// This loop creates all the ROWS based on the number of items in workHours array
+for (let i = 0; i < workHours.length; i++) {
+    // Creates a new ROW that will have the workHour, Input, and saveButton
+    let newRow = $("<section>").addClass("row");
 
-// Creates the <div> in the new ROW that will display the workHour
-let timeColumn = $("<div>").addClass("col-md-2 hour time-block");
-timeColumn.text(workHours[0]); //previously 'time'
-newRow.append(timeColumn);
+    // Creates the <div> in the new ROW that will display the workHour
+    let timeColumn = $("<div>").addClass("col-md-2 hour time-block");
+    timeColumn.text(workHours[i]); //previously 'time'
+    newRow.append(timeColumn);
 
-// Creates the <div> in the new ROW that will display the text input area
-let inputColumn = $("<div>").addClass("col-md-8");
-let input = $('<input type = "text">');
-input.addClass("input textarea");
-input.attr("id", workHours[0]); //previously workHours was 'time'
-inputColumn.append(input);
-newRow.append(inputColumn);
+    // Creates the <div> in the new ROW that will display the text input area
+    let inputColumn = $("<div>").addClass("col-md-8");
+    let input = $('<input type = "text">');
+    input.addClass("input textarea");
+    input.attr("id", workHours[i]);
+    inputColumn.append(input);
+    newRow.append(inputColumn);
 
-// Creates the <div> in the new ROW that will display the saveButton
-let buttonColumn = $("<div>").addClass("col-md-2");
-let saveButton = $("<button>").addClass("save-btn saveBtn");
-saveButton.attr("date-time", workHours[0]); //previously workHours was 'time'
-saveButton.text("Save");
-buttonColumn.append(saveButton);
-newRow.append(buttonColumn);
+    // Creates the <div> in the new ROW that will display the saveButton
+    let buttonColumn = $("<div>").addClass("col-md-2");
+    let saveButton = $("<button>").addClass("save-btn saveBtn");
+    saveButton.attr("date-time", workHours[i]); 
+    saveButton.text("Save");
+    buttonColumn.append(saveButton);
+    newRow.append(buttonColumn);
 
-// Displays the new ROW (i.e. workHour, inputText, saveButton)
-container.append(newRow);
+    // Displays the new ROW (i.e. workHour, inputText, saveButton)
+    container.append(newRow);
+
+}
