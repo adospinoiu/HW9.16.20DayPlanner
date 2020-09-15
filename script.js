42deb58c3d974827115236b540a544be55dcfc50
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2020-09-15 05:44:33 -0700

    Turned the workHour array into an array of objects to be able to compare the hour

--- script.js
+++ script.js
@@ -8,17 +8,69 @@ let array = [];
 array.push("Current time: " + currentTime);
 console.log(array);
 
+
+// Splits the currentTime components into three: day, date, time
 let timeCompare = " ";
 timeCompare = currentTime.split(", ");
 console.log("Split current time: " + timeCompare);
 console.log("Hour: " + timeCompare[2]);
 
+
+// Splits the time component and store just the hour 05, 06, etc
 let hourCompare = [];
 hourCompare = timeCompare[2].split(":");
 console.log("Hour Split: " + hourCompare[0]);
 
+
 // Array of hours considered workHours that will be displayed
-let workHours = ["07:00 AM", "08:00 AM", "09:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "01:00 PM", "02:00 PM", "03:00 PM", "04:00 PM"];
+// let workHours = [
+//     { 07: ":00 AM" },
+//     { 08: ":00 AM" },
+//     { 09: ":00 AM" },
+//     { 10: ":00 AM" },
+//     { 11: ":00 AM" },
+//     { 12: ":00 PM" },
+//     { 01: ":00 PM" },
+//     { 02: ":00 PM" },
+//     { 03: ":00 PM" },
+//     { 04: ":00 PM" }
+// ]
+
+
+let workHours = [
+    { 
+        hour: "07",
+        minutes: ":00 AM" },
+    { 
+        hour: "08",
+        minutes: ":00 AM" },
+    {
+        hour: "09",
+        minutes: ":00 AM" },
+    { 
+        hour: "10",
+        minutes: ":00 AM" },
+    { 
+        hour: "11",
+        minutes: ":00 AM" },
+    { 
+        hour: "12",
+        minutes: ":00 PM" },
+    { 
+        hour: "01",
+        minutes: ":00 PM" },
+    { 
+        hour: "02",
+        minutes: ":00 PM" },
+    { 
+        hour: "03",
+        minutes: ":00 PM" },
+    { 
+        hour: "04", 
+        minutes: ":00 PM" }
+]
+
+
 
 // Links to the 'container' <div> on the html. ***Delete CLASS***
 let container = $(".container").addClass("past");
@@ -30,21 +82,21 @@ for (let i = 0; i < workHours.length; i++) {
 
     // Creates the <div> in the new ROW that will display the workHour
     let timeColumn = $("<div>").addClass("col-md-2 hour time-block");
-    timeColumn.text(workHours[i]); //previously 'time'
+    timeColumn.text(workHours[i].hour + workHours[i].minutes); //previously 'time'
     newRow.append(timeColumn);
 
     // Creates the <div> in the new ROW that will display the text input area
     let inputColumn = $("<div>").addClass("col-md-8");
     let input = $('<input type = "text">');
     input.addClass("input textarea");
-    input.attr("id", workHours[i]);
+    input.attr("id", workHours[i].hour);
     inputColumn.append(input);
     newRow.append(inputColumn);
 
     // Creates the <div> in the new ROW that will display the saveButton
     let buttonColumn = $("<div>").addClass("col-md-2");
     let saveButton = $("<button>").addClass("save-btn saveBtn");
-    saveButton.attr("date-time", workHours[i]); 
+    saveButton.attr("date-time", workHours[i].hour);
     saveButton.text("Save");
     buttonColumn.append(saveButton);
     newRow.append(buttonColumn);
